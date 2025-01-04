a84e60d4c5d43133730375dbaea3beaee33520fe
Eduardo Ridao <manoguar@msn.com>
2025-01-04 01:09:15 +0100

    transitioning to v0.2

---
+++ src/ARWNI2S.Backend.Data/INiisStartup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ARWNI2S.Backend.Data
+{
+    public interface INiisStartup
+    {
+        int Order { get; }
+
+        void ConfigureServices(IServiceCollection services, IConfiguration configuration);
+
+        void ConfigureApplication(IApplicationBuilder applicationBuilder);
+    }
+}
