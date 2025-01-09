90405609eba90571613c13adb08c1511fb63f0f9
Eduardo Ridao <manoguar@msn.com>
2025-01-09 02:48:26 +0100

    commit

---
+++ src/Framework/ARWNI2S.Runtime.Narrator/ConfigureOrleansSilo.cs
@@ -0,0 +1,22 @@
+﻿using ARWNI2S.Engine.Builder;
+using ARWNI2S.Extensibility;
+using ARWNI2S.Lifecycle;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ARWNI2S.Runtime.Narrator
+{
+    internal class ConfigureOrleansSilo : IConfigureEngine
+    {
+        public int Order => NI2SLifecycleStage.CoreInitialize;
+
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+
+        public void ConfigureEngine(IEngineBuilder engineBuilder)
+        {
+        }
+    }
+}
---
+++ src/Framework/ARWNI2S.Runtime.Narrator/Storage/ActorState.cs
@@ -0,0 +1,17 @@
+﻿using ARWNI2S.Engine.Data;
+
+namespace ARWNI2S.Runtime.Narrator.Storage
+{
+    public sealed class ActorState : DataEntity
+    {
+        /// <summary>
+        /// Gets or sets the state binary
+        /// </summary>
+        public byte[] BinaryData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the picture identifier
+        /// </summary>
+        public int ActorId { get; set; }
+    }
+}
